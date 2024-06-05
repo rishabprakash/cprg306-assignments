eec77311dcec5788fcb00beb4ab808934a57e755
rishabprakash <141350228+rishabprakash@users.noreply.github.com>
2024-06-04 18:21:56 -0600

    changed something

--- app/page.js
+++ app/page.js
@@ -5,8 +5,8 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
      <h1>CPRG 306: Web Development 2 - Assignments</h1>
-     <Link href="week-2">Week-2</Link>
-     <Link href="week-3">Week-3</Link>
+     <Link href="week-2">Week - 2</Link>
+     <Link href="week-3">Week - 3</Link>
     </main>
   );
 }
